a8cc1f4e413891fd356a621e7a09db9aa2618fe0
cphilliplierz <cplierz@gmail.com>
2014-06-26 10:21:30 -0700

    Signed-off-by: cphilliplierz <cplierz@gmail.com>

---
+++ Homework/Gaddis_7thEd_Chap2_Prob1_Adding_Variables/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: C. Phillip Lierz
+ * Purpose: Day Four Classwork
+ * Created on June 26, 2014, 10:00 AM
+ */
+
+//System Libraries
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution
+int main(int argc, char** argv) {
+
+    //Define Variables
+    unsigned short A=62;
+    unsigned short B=99;
+    unsigned short Total;
+
+    //Calculations
+    Total=A+B;
+
+    //Output
+    cout<<"The total of the variables A and B is "<<Total<<endl;
+    
+    return 0;
+}
+
