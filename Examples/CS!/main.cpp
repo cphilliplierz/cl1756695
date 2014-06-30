0cbe71aa00ff7a87ecb5e2489639d4aafe8b6cc5
cphilliplierz <cplierz@gmail.com>
2014-06-30 14:02:06 -0700

    Signed-off-by: cphilliplierz <cplierz@gmail.com>

--- Examples/CS!/main.cpp
+++ Examples/CS!/main.cpp
@@ -21,17 +21,21 @@ using namespace std;
 int main(int argc, char** argv) {
     
     //Output blown-up lettering
-    cout<<"      CCCC              SSSS         !!"<<endl;
-    cout<<"   CCC    CCC         SS    SS       !!"<<endl;
-    cout<<" CC          CC      S        S      !!"<<endl;
-    cout<<"C                    S               !!"<<endl;
-    cout<<"C                     SS             !!"<<endl;
-    cout<<"C                       SSSS         !!"<<endl;
-    cout<<"C                           SS       !!"<<endl;
-    cout<<" CC          CC      S        S      !!"<<endl;
-    cout<<"   CCC    CCC         SS    SS"<<endl;
-    cout<<"      CCCC              SSSS         00"<<endl;
-    
+    cout<<"             C                      S"<<endl;
+    cout<<"            CCCC  C                SSS       !!"<<endl;
+    cout<<"      CC   CCCCCCC           SSS    S        !!"<<endl;
+    cout<<"   CCC    CC |              S   SS   SS      !!"<<endl;
+    cout<<" CCCC  CCC   |           S SS    SSS  S      !!"<<endl;
+    cout<<"CCCC     CC__|           S  SSS   SSSS       !!"<<endl;
+    cout<<"CCC      CC  |           SS  SSS   S         !!"<<endl;
+    cout<<"CCCC     CC  |            SSS  SSS           !!"<<endl;
+    cout<<"CCCC     CC  |              SSS   SSSS       !!"<<endl;
+    cout<<"CCCC     CC  |                SSSS   SSS     !!"<<endl;
+    cout<<" CCC     CC  |    C             SSS   SS     !!"<<endl;
+    cout<<"  CCC    CC  |  CC               S    S      !!"<<endl;
+    cout<<"    CCC  C   |CC          SSSSSS     S       !!"<<endl;
+    cout<<"      CCCCCCC|           SS   SSSSSSS  "<<endl;
+    cout<<"             |          S        S           00"<<endl;
             
     return 0;
 }
