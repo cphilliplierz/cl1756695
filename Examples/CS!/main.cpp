dd7cfff75c3f064e0fc24bfa006639401ed2515f
cphilliplierz <cplierz@gmail.com>
2014-06-30 11:42:33 -0700

    Signed-off-by: cphilliplierz <cplierz@gmail.com>

---
+++ Examples/CS!/main.cpp
@@ -0,0 +1,38 @@
+/* 
+ * File:   main.cpp
+ * Author: C. Phillip Lierz
+ * Purpose: Practice
+ * Created on June 30, 2014, 11:18 AM
+ */
+
+//System Libraries
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Starts Here!
+
+int main(int argc, char** argv) {
+    
+    //Output blown-up lettering
+    cout<<"      CCCC              SSSS         !!"<<endl;
+    cout<<"   CCC    CCC         SS    SS       !!"<<endl;
+    cout<<" CC          CC      S        S      !!"<<endl;
+    cout<<"C                    S               !!"<<endl;
+    cout<<"C                     SS             !!"<<endl;
+    cout<<"C                       SSSS         !!"<<endl;
+    cout<<"C                           SS       !!"<<endl;
+    cout<<" CC          CC      S        S      !!"<<endl;
+    cout<<"   CCC    CCC         SS    SS"<<endl;
+    cout<<"      CCCC              SSSS         00"<<endl;
+    
+            
+    return 0;
+}
+
