e280aca1d323e37c34298e3c491feb5e842a0a3e
cphilliplierz <cplierz@gmail.com>
2014-07-15 11:37:21 -0700

    Signed-off-by: cphilliplierz <cplierz@gmail.com>

---
+++ Homework/Assignment 4/Assignment_4/main.cpp
@@ -0,0 +1,133 @@
+/* 
+ * File:   main.cpp
+ * Author: C. Phillip Lierz
+ * Purpose: Assignment 4 Gaddis 7th Edition Chapter 5 Problems 1-10
+ * Created on July 15, 2014, 10:29 AM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution
+int main(int argc, char** argv) {
+    cout<<"Hello!\nThis is Assignment 4."<<endl<<endl;
+    cout<<"Problem One: Sum of Numbers."<<endl;
+    
+    //Declare Variables
+    unsigned int posint;//Positive integer input by user
+    unsigned int sumint=0;//Sum of the posint and all its subordinate integers
+    
+    //Inputs
+    cout<<"Input a positive integer"<<endl;
+    cin>>posint;
+    
+    //Calculations
+    for(int count=0; count<=posint; count++)
+    {
+        sumint+=count;
+    }
+    
+    //Outputs
+    cout<<"The sum of the positive integer you inputed and all the positive ";
+    cout<<"integers of lesser value to it is "<<sumint<<"."<<endl<<endl;
+    
+    cout<<"Problem Two: Characters for the ASCII Codes."<<endl;
+    
+    //Declare Variables
+    
+    //Inputs
+    
+    //Calculations
+    
+    //Outputs
+    
+    cout<<"Problem Three: Ocean Levels."<<endl;
+    
+    //Declare Variables
+    
+    //Inputs
+    
+    //Calculations
+    
+    //Outputs
+    
+    cout<<"Problem Four: Calories Burned."<<endl;
+    
+    //Declare Variables
+    
+    //Inputs
+    
+    //Calculations
+    
+    //Outputs
+    
+    cout<<"Problem Five: Membership Fees Increase."<<endl;
+    
+    //Declare Variables
+    
+    //Inputs
+    
+    //Calculations
+    
+    //Outputs
+    
+    cout<<"Problem Six: Distance Traveled."<<endl;
+    
+    //Declare Variables
+    
+    //Inputs
+    
+    //Calculations
+    
+    //Outputs
+    
+    cout<<"Problem Seven: Pennies for Pay."<<endl;
+    
+    //Declare Variables
+    
+    //Inputs
+    
+    //Calculations
+    
+    //Outputs
+    
+    cout<<"Problem Eight: Math Tutor."<<endl;
+    
+    //Declare Variables
+    
+    //Inputs
+    
+    //Calculations
+    
+    //Outputs
+    
+    cout<<"Problem Nine: Horel Occupancy."<<endl;
+    
+    //Declare Variables
+    
+    //Inputs
+    
+    //Calculations
+    
+    //Outputs
+    
+    cout<<"Problem Ten: Average Rainfall."<<endl;
+    
+    //Declare Variables
+    
+    //Inputs
+    
+    //Calculations
+    
+    //Outputs
+    
+    return 0;
+}
+
